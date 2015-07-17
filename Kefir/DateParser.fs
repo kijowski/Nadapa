e63dd4100391e75ad30b50e641725f37ca8086bc
Michal Kijowski <michal.kijowski@hotmail.com>
2015-07-17 01:44:39 +0100

    Added files missed by Xamarin

---
+++ Kefir/DateParser.fs
@@ -0,0 +1,89 @@
+﻿namespace Kefir
+open System
+open FParsec
+
+
+type DateTransform = DateTime -> DateTime
+
+type ParseResult =
+    | SuccessfulParse of DateTime
+    | FailedParse of string
+
+type AbsoluteShift =
+    { 
+        ShiftInDays:int
+        Labels : string seq
+    }
+
+module ParserCreators = 
+    let createParser dateTransform names =
+        names 
+        |> Seq.map (fun name -> stringCIReturn name dateTransform  .>> spaces )
+        |> choice
+    let createConsumingParser names =
+        names 
+        |> Seq.map (fun name -> skipStringCI name  .>> spaces )
+        |> choice
+
+module InternalParsers = 
+    let todayP = 
+        ParserCreators.createParser (id) ["today"; "tdy" ; "now"] 
+    let tomorrowP = 
+        ParserCreators.createParser (fun (date:DateTime) -> date.AddDays(1.)) ["tomorow";"tomorrow";"tommorrow";"tommorow";"tmr"] 
+    let yesterdayP = 
+        ParserCreators.createParser (fun (date:DateTime) -> date.AddDays(-1.)) ["yesterday"; "yest" ; "ye"]     
+
+    let shiftP (shiftType:AbsoluteShift) = 
+        pint32 
+        .>> spaces 
+        .>> (ParserCreators.createConsumingParser shiftType.Labels) 
+        |>> fun shiftSize -> float(shiftSize * shiftType.ShiftInDays)
+
+    let fromP (shiftType:AbsoluteShift) parser= 
+        shiftP shiftType
+        .>> spaces 
+        .>>? ParserCreators.createConsumingParser ["from"; "after"]
+        .>> spaces 
+        .>>. parser
+        |>> fun (shift, dateTransf) -> ((fun (date:DateTime) -> date.AddDays(shift)) >> dateTransf)
+    
+    let beforeP (shiftType:AbsoluteShift) parser = 
+        shiftP shiftType
+        .>> spaces 
+        .>>? ParserCreators.createConsumingParser ["before"]
+        .>> spaces 
+        .>>. parser
+        |>> fun (shift, dateTransf) -> ((fun (date:DateTime) -> date.AddDays(-shift)) >> dateTransf)
+
+    let agoP (shiftType:AbsoluteShift) = 
+        shiftP shiftType
+        .>> spaces 
+        .>>? ParserCreators.createConsumingParser ["ago"]
+        |>> fun (shift) -> ((fun (date:DateTime) -> date.AddDays(-shift)))
+
+
+type DateParser(?baseDate : DateTime) = 
+    let bDate = defaultArg baseDate DateTime.Now  
+
+
+    let combinedParser = 
+        let pars, refPar = createParserForwardedToRef()      
+
+        refPar := choice [
+            InternalParsers.todayP 
+            InternalParsers.tomorrowP
+            InternalParsers.yesterdayP
+            InternalParsers.agoP {ShiftInDays = 1 ; Labels = ["days"; "day"]}
+            InternalParsers.fromP {ShiftInDays = 1 ; Labels = ["days"; "day"]} pars
+            InternalParsers.beforeP {ShiftInDays = 1 ; Labels = ["days"; "day"]} pars
+        ]
+        pars
+    
+         
+    member this.Parse(arg:string) =
+        match run (combinedParser) arg with
+            | Success (result, _, __) -> SuccessfulParse(result bDate)
+            | Failure(x,y,z) -> FailedParse(x)
+
+            
+
---
+++ Kefir/Script.fsx
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+#I @"/Users/michal/Code/Kefir/packages/Newtonsoft.Json.6.0.5/lib/net45/"
+#r @"/Users/michal/Code/Kefir/packages/Newtonsoft.Json.6.0.5/lib/net45/Newtonsoft.Json.dll"
+
+#I @"/Users/michal/Code/Kefir/packages/FSharp.Data.2.2.5/lib/net40/"
+#r @"FSharp.Data.dll"
+
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+let x = JsonValue.Parse("""{"absolute":[{"millenium":"20000101"}]}""")
+x?absolute.AsArray() |> Seq.iter (printfn "%A")
+
+
+#I @"/Users/michal/Code/Kefir/packages/FParsec.1.0.1/lib/net40-client"
+#r @"FParsecCS.dll"
+#r @"FParsec.dll"
+
+#load "DateParser.fs"
+open Kefir
+let sut = DateParser()
+sut.Parse("today")
+sut.Parse("3 days before now")
