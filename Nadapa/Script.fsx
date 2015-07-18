aa5c14fc30710f1d7cc9e780f9dea3cc87543e92
Michal Kijowski <michal.kijowski@hotmail.com>
2015-07-18 13:04:21 +0100

    Missed changes after renaming

--- Nadapa/Script.fsx
+++ Nadapa/Script.fsx
@@ -1,9 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
-#I @"/Users/michal/Code/Kefir/packages/Newtonsoft.Json.6.0.5/lib/net45/"
-#r @"/Users/michal/Code/Kefir/packages/Newtonsoft.Json.6.0.5/lib/net45/Newtonsoft.Json.dll"
+#I @"/Users/michal/Code/Nadapa/packages/Newtonsoft.Json.6.0.5/lib/net45/"
+#r @"/Users/michal/Code/Nadapa/packages/Newtonsoft.Json.6.0.5/lib/net45/Newtonsoft.Json.dll"
 
-#I @"/Users/michal/Code/Kefir/packages/FSharp.Data.2.2.5/lib/net40/"
+#I @"/Users/michal/Code/Nadapa/packages/FSharp.Data.2.2.5/lib/net40/"
 #r @"FSharp.Data.dll"
 
 open FSharp.Data
@@ -13,7 +13,7 @@ let x = JsonValue.Parse("""{"absolute":[{"millenium":"20000101"}]}""")
 x?absolute.AsArray() |> Seq.iter (printfn "%A")
 
 
-#I @"/Users/michal/Code/Kefir/packages/FParsec.1.0.1/lib/net40-client"
+#I @"/Users/michal/Code/Nadapa/packages/FParsec.1.0.1/lib/net40-client"
 #r @"FParsecCS.dll"
 #r @"FParsec.dll"
 
