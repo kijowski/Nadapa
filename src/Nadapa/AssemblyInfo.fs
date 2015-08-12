21541477747d2775a08f7542e83600c4721f8793
Michal Kijowski <michal.kijowski@hotmail.com>
2015-08-12 21:22:49 +0100

    Increase version to 1.0.0 in AssemblyInfo

--- src/Nadapa/AssemblyInfo.fs
+++ src/Nadapa/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Nadapa")>]
 [<assembly: AssemblyProductAttribute("Nadapa")>]
 [<assembly: AssemblyDescriptionAttribute("Simple micro-library for human readable dates parsing.")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.3"
+    let [<Literal>] Version = "1.0.0"
